b50036c506c0afef982898bcde0a66e8a52b0853
AbrahamLugoRamirez07 <abraham.lugo@integrait.co>
2026-03-22 11:09:47 -0500

    feat: refactor PaymentsService and TransactionController to use environment variables for sensitive data

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Checkout API')
     .setDescription('API for payment processing')
